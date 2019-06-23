ffa8e31ee97b021a18ca48e71887b108d372df46
philipkariuki <kariukiphil@gmail.com>
2019-06-24 02:25:46 +0300

    updates models.py creates Source and Article classes

---
+++ app/models.py
@@ -0,0 +1,30 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,id,title,overview,category):
+        self.id =id
+        self.title = title
+        self.overview = overview
+        self.category = category    
+
+
+
+
+
+class Article:
+    '''
+    Articles class
+    
+    '''
+   
+
+    def __init__(self,article_id,title,description,author):
+        self.article_id = article_id
+        self.title = title
+        self.description = description
+        self.author = author
+
+
+    
